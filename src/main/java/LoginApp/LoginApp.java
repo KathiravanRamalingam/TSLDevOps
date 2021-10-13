5357ac5df1ccf4675a425de93b9d341aaf77779c
KathiravanRamalingam <kathiravan.1708010@srec.ac.in>
2021-10-13 15:29:12 +0530

    My first Commit

---
+++ src/main/java/LoginApp/LoginApp.java
@@ -0,0 +1,22 @@
+package LoginApp;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class LoginApp {
+	
+	public boolean userLogin(String user, String u_pwd) throws Exception{
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("src\\test\\resources\\credentials.property"));
+		String un = prop.getProperty("username");
+		String password = prop.getProperty("password");
+		
+		if(un.equals(user) && password.equals(u_pwd)) 
+			return true;
+		else
+			return false;
+			
+		
+	}
+
+}
---
+++ src/test/java/LoginTest/NumberComparison.java
@@ -0,0 +1,11 @@
+package LoginTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NumberComparison {
+  @Test
+  public void numberComp() {
+	  Assert.assertTrue(5>4);
+  }
+}
